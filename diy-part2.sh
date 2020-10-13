15e30807ac53ccf029446e74ef06ace25cdc92ad
siryle1213 <52944877+siryle1213@users.noreply.github.com>
2020-10-13 16:55:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
+git clone https://github.com/liuran001/openwrt-packages package
